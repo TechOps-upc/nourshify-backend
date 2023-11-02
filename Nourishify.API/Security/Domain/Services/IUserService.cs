c1b919c08d330d2685933100a1f45f22c7a839bd
Adrian Melgar <DaikiMT@outlook.es>
2023-11-02 07:22:07 -0500

    Implemented services

--- Nourishify.API/Security/Domain/Services/IUserService.cs
+++ Nourishify.API/Security/Domain/Services/IUserService.cs
@@ -1,6 +1,15 @@
+using Nourishify.API.Security.Domain.Models;
+using Nourishify.API.Security.Domain.Services.Communication;
+
 namespace Nourishify.API.Security.Domain.Services;
 
 public interface IUserService
 {
-    
+    Task<AuthenticateResponse> Authenticate(AuthenticateRequest model);
+    Task<IEnumerable<User>> ListAsync();
+    Task<User> GetByIdAsync(int id);
+    Task<User> GetByIdAsyncV2(int id);
+    Task RegisterAsync(RegisterRequest model);
+    Task UpdateAsync(int id, UpdateRequest model);
+    Task DeleteAsync(int id);
 }
