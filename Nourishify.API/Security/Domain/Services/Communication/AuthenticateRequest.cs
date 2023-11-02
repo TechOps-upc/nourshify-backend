06fbfaaab866e9c138bad99ceeaef2317adcc965
Adrian Melgar <DaikiMT@outlook.es>
2023-11-02 07:19:00 -0500

    Implemented responses

--- Nourishify.API/Security/Domain/Services/Communication/AuthenticateRequest.cs
+++ Nourishify.API/Security/Domain/Services/Communication/AuthenticateRequest.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Nourishify.API.Security.Domain.Services.Communication;
 
 public class AuthenticateRequest
 {
-    
+    [Required] public string Username { get; set; }
+
+    [Required] public string Password { get; set; }
 }
--- Nourishify.API/Security/Domain/Services/Communication/AuthenticateResponse.cs
+++ Nourishify.API/Security/Domain/Services/Communication/AuthenticateResponse.cs
@@ -2,5 +2,10 @@ namespace Nourishify.API.Security.Domain.Services.Communication;
 
 public class AuthenticateResponse
 {
-    
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public int RoleId { get; set; }
+    public string Token { get; set; }
 }
