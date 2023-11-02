1274da4968086bbd4da9d441f4cd0e6129e482e0
Adrian Melgar <DaikiMT@outlook.es>
2023-11-02 07:21:02 -0500

    Implemented requests

--- Nourishify.API/Security/Domain/Services/Communication/RegisterRequest.cs
+++ Nourishify.API/Security/Domain/Services/Communication/RegisterRequest.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Nourishify.API.Security.Domain.Services.Communication;
 
 public class RegisterRequest
 {
-    
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public int RoleId { get; set; }
 }
--- Nourishify.API/Security/Domain/Services/Communication/UpdateRequest.cs
+++ Nourishify.API/Security/Domain/Services/Communication/UpdateRequest.cs
@@ -2,5 +2,9 @@ namespace Nourishify.API.Security.Domain.Services.Communication;
 
 public class UpdateRequest
 {
-    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int RoleId { get; set; }
 }
