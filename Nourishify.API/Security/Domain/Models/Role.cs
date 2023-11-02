945d907988d0abdd628ee212cbb9b395d92139db
Adrian Melgar <DaikiMT@outlook.es>
2023-11-02 09:52:25 -0500

    Added security bc to appdbcontext

--- Nourishify.API/Security/Domain/Models/Role.cs
+++ Nourishify.API/Security/Domain/Models/Role.cs
@@ -4,4 +4,7 @@ public class Role
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    
+    //Relationship
+    public IList<User> Users { get; set; } = new List<User>();
 }
--- Nourishify.API/Shared/Persistence/Context/AppDbContext.cs
+++ Nourishify.API/Shared/Persistence/Context/AppDbContext.cs
@@ -11,4 +11,30 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // SECURITY
+        builder.Entity<Role>().ToTable("Roles");
+        builder.Entity<Role>().HasKey(r => r.Id);
+        builder.Entity<Role>().Property(r => r.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Role>().Property(r => r.Name).IsRequired().HasMaxLength(30);
+        
+        // Role - User relationship
+        builder.Entity<Role>()
+            .HasMany(r => r.Users)
+            .WithOne(u => u.Role)
+            .HasForeignKey(u => u.RoleId);
+        
+        // Constraints
+        builder.Entity<User>().ToTable("Users");
+        builder.Entity<User>().HasKey(p => p.Id);
+        builder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<User>().Property(p => p.Username).IsRequired().HasMaxLength(30);
+        builder.Entity<User>().Property(p => p.FirstName).IsRequired();
+        builder.Entity<User>().Property(p => p.LastName).IsRequired();
+        
+    }
 }
